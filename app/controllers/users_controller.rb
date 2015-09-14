c052d817638f25ff6827d07593b684a005d338af
nekocreate <nekocreate7@gmail.com>
2015-09-14 00:45:25 +0000

    kadai edit user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   
   def show
-    @user = User.find(params[:id])  
+    @user = User.find(params[:id])
+    # @users = User.all
+  
+
   end
   
   def new
@@ -19,9 +22,29 @@ class UsersController < ApplicationController
     end
   end
   
+
+def edit
+  @user = User.find(params[:id])
+end
+
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # 保存に成功した場合はTopページへリダイレクト
+      redirect_to @user , notice: 'プロフィールを編集しました'
+    else
+      # 保存に失敗した場合は、編集画面に戻す
+      render 'edit'
+    end
+  end
+
+
+  
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    # params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :area, :profile, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
     has_secure_password
+    validates :profile, presence: true, length: { maximum: 255 }
+    validates :password, length: { minimum: 6 } , on: :create
+    validates :password, length: { minimum: 6 } , on: :update, allow_blank: true
 end
